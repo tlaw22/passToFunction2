8a41ede2949e2f4abc16dfd170f32d64d0647b33
Tim Lawless <108081811+tlaw22@users.noreply.github.com>
2023-07-17 10:54:03 -0400

    Update index.js
    added array slice

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ sayThanks('Cole');
 Test comment
 
 */
+
+
+// Example os .shift() and .unshift()
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift();
+groceryList.unshift('popcorn');
+console.log(groceryList);
+console.log (groceryList.slice(1, 4));
+
